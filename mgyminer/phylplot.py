a934c76a8e4b5e112ff4d963d60fae57fd1e1e1d
Felix Langer <felix@ebi.ac.uk>
2021-06-03 15:25:29 +0100

    add prototype for domain ploting in phylogenetic tree

--- mgyminer/phylplot.py
+++ mgyminer/phylplot.py
@@ -1,6 +1,7 @@
 import math
 
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
 from Bio import Phylo
 
@@ -148,6 +149,39 @@ def _filter_dict(metadata, parameter):
     return pd.Series(list(metadata[parameter]), index=metadata["dom_acc"]).to_dict()
 
 
+def domain_colors(domains):
+    """
+    function to create a domain : color dictionary. The colors will be based on
+    the dark24 color palette. If the number of domains is greater than the length
+    of the palette the colors will start again from the beginning.
+    :param domains:
+    :return:
+    """
+    # color palette from plotly 10 colors, if more are needed dark24
+    palette = px.colors.qualitative.D3
+    domains = set(domains)
+    colors = []
+    for i in range(len(domains)):
+        colors.append(palette[(i) % len(palette)])
+    return dict(zip(domains, colors))
+
+
+def domain_start(x_coords):
+    return max(x_coords.values()) + 0.5
+
+
+def smallest_y_dist(protein_ys):
+    ys = sorted(protein_ys.values())
+    prev = ys[0]
+    smallest_dist = math.inf
+    for y in ys[1:]:
+        dist = abs(prev - y)
+        if smallest_dist > dist:
+            smallest_dist = dist
+        prev = y
+    return smallest_dist
+
+
 def plot_tree(args):
     metadata = pd.read_csv(args.filter)
     tree = Phylo.read(args.tree, "newick")
@@ -160,21 +194,19 @@ def plot_tree(args):
         return f"{row['target_name']}{domain}/{row['env_from']}-{row['env_to']}"
 
     metadata["dom_acc"] = metadata.apply(lambda row: _idpluscoords(row), axis=1)
-    # TODO find way to get the name of the query sequence not from arguments but somewhere else to reduce number
-    #  of flags
     query = metadata["query_name"][0]
 
-    min = 0 if args.min is None else args.min
-    max = math.inf if args.max is None else args.max
+    minimum = 0 if args.min is None else args.min
+    maximum = math.inf if args.max is None else args.max
 
     if args.param is not None:
         # if parameter is specified find proteins that match filter for parameter
         filter_dict = _filter_dict(metadata, args.param)
-        of_interest = _in_thresholds(filter_dict, min, max)
+        of_interest = _in_thresholds(filter_dict, minimum, maximum)
     else:
         # if no parameter is given take the tree distance
         filter_dict = distances(tree, query)
-        of_interest = _in_thresholds(filter_dict, min, max)
+        of_interest = _in_thresholds(filter_dict, minimum, maximum)
 
     x_coords = get_x_coordinates(tree)
     y_coords = get_y_coordinates(tree)
@@ -230,6 +262,137 @@ def plot_tree(args):
         hoverinfo="text",
     )
 
+    if "Pfams" in metadata.columns:
+        # Choose color scheme for protein domains
+        protein_ys = {}
+
+        for node in range(0, len(nodes["x"])):
+            if nodes["text"][node]:
+                protein_ys[nodes["text"][node].split("<br>")[0]] = nodes["y"][node]
+
+        smallest_y_dist(protein_ys)
+
+        shapedata = {
+            "type": [],
+            "x0": [],
+            "y0": [],
+            "x1": [],
+            "y1": [],
+            "fillcolor": [],
+            "layer": [],
+            "line_width": [],
+        }
+
+        dom_hover_x = []
+        dom_hover_y = []
+        dom_hover_fill = []
+        dom_hover_text = []
+
+        # Build mapping of Pfam accs to domain name
+        dom_names = [
+            name
+            for arch in metadata["domain_names"].tolist()
+            for name in arch.split("~")
+        ]
+        dom_accs = [
+            name for arch in metadata["Pfams"].tolist() for name in arch.split("-")
+        ]
+        acc_to_name = dict(zip(dom_accs, dom_names))
+
+        # Dict with protein and its domains
+        pfams = _filter_dict(metadata, "Pfams")
+        pfams = {k: v.split("-") for k, v in pfams.items()}
+
+        # Get unique pfam domains in all hits
+        unique_domains = set(
+            [domain for domains in list(pfams.values()) for domain in domains]
+        )
+
+        baseline = domain_start(x_coords)
+        smallest_y = smallest_y_dist(protein_ys)
+        y_gap = smallest_y / 10
+        half_height = (smallest_y - y_gap) / 2
+
+        # domain section shouldnt take more than 1/3 of the plot size,
+        # assuming we dont find more than 10 domains for a normal protein
+        domain_width = (baseline / 3) / 10
+        x_gab = domain_width / 2
+
+        # dict with distinc color for each unique pfam
+        pfam_colors = domain_colors(unique_domains)
+
+        for protein in protein_ys.keys():
+            start = baseline
+            if protein == query:
+                pass
+            else:
+                # Draw domain by domain
+                for domain in pfams[protein]:
+                    # Draw shape
+                    x0 = start
+                    x1 = start + domain_width
+                    y0 = protein_ys[protein] - half_height
+                    y1 = protein_ys[protein] + half_height
+                    shapedata["type"].append("rect")
+                    shapedata["x0"].append(x0)
+                    shapedata["y0"].append(y0)
+                    shapedata["x1"].append(x1)
+                    shapedata["y1"].append(y1)
+                    shapedata["fillcolor"].append(pfam_colors[domain])
+                    shapedata["layer"].append("above")
+                    shapedata["line_width"].append(0)
+
+                    # Draw invisible glyph for hover info
+                    dom_hover_x.append([x0, x1, x1, x0, x0])
+                    dom_hover_y.append([y0, y0, y1, y1, y0])
+                    dom_hover_fill.append(pfam_colors[domain])
+                    dom_hover_text.append(
+                        f"<br>Pfam Accession: {domain} <br>Name: {acc_to_name[domain]}"
+                    )
+
+                    start = start + domain_width + x_gab
+                # Add a straight line to conncect domains
+                shapedata["type"].append("rect")
+                shapedata["x0"].append(baseline + x_gab)
+                shapedata["y0"].append(protein_ys[protein])
+                shapedata["x1"].append(start - (domain_width + x_gab))
+                shapedata["y1"].append(protein_ys[protein])
+                shapedata["fillcolor"].append("black")
+                shapedata["layer"].append("below")
+                shapedata["line_width"].append(5)  # Might need to be variable
+
+        domain_shapes = [
+            go.layout.Shape(
+                type=shapedata["type"][i],
+                x0=shapedata["x0"][i],
+                x1=shapedata["x1"][i],
+                y0=shapedata["y0"][i],
+                y1=shapedata["y1"][i],
+                fillcolor=shapedata["fillcolor"][i],
+                opacity=1,
+                layer=shapedata["layer"][i],
+                line_width=shapedata["line_width"][i],
+            )
+            for i in range(len(shapedata["x0"]))
+        ]
+        shapes = line_shapes + domain_shapes
+
+        dom_hover_data = dict(
+            type="scatter",
+            fill="toself",
+            x=dom_hover_x,
+            y=dom_hover_y,
+            mode="lines",
+            name="",
+            opacity=1,
+            fillcolor=dom_hover_fill,
+            text=dom_hover_text,
+            hoverinfo="text",
+        )
+
+    else:
+        shapes = line_shapes
+
     layout = dict(
         title=f"Phylogeny of {query} against MGnify proteins",
         font=dict(family="Balto", size=14),
@@ -249,7 +412,23 @@ def plot_tree(args):
         hovermode="closest",
         plot_bgcolor="rgb(250,250,250)",
         margin=dict(l=10),
-        shapes=line_shapes,  # lines for tree branches
+        shapes=shapes,  # lines for tree branches
     )
     f = go.Figure(data=[nodes], layout=layout)
+
+    if "Pfams" in metadata.columns:
+        for i in range(len(dom_hover_data["text"])):
+            f.add_trace(
+                go.Scatter(
+                    x=dom_hover_data["x"][i],
+                    y=dom_hover_data["y"][i],
+                    fill="toself",
+                    mode="lines",
+                    name="",
+                    text=dom_hover_data["text"][i],
+                    opacity=0,
+                    fillcolor=dom_hover_data["fillcolor"][i],
+                )
+            )
+
     f.write_html("tree_vis.html")
