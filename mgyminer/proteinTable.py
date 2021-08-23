76984063af76c35ea4cf4430b70d916a88ec1299
Felix Langer <felix@ebi.ac.uk>
2021-08-24 00:17:50 +0100

    add sort method to proteinTable

--- mgyminer/proteinTable.py
+++ mgyminer/proteinTable.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Union
 
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
@@ -19,18 +20,90 @@ class proteinTable:
         return self._df
 
     @df.setter
-    def df(self, results) -> DataFrame:
+    def df(self, results: Union[DataFrame, str, Path]) -> DataFrame:
         """read the HMMER Domain Table file into a Pandas dataframe"""
         if isinstance(results, str):
             results = Path(results)
 
         if isinstance(results, DataFrame):
             self._df = results
-
         elif isinstance(results, Path):
             self._df = pd.read_csv(results)
         else:
-            self._df = pd.DataFrame()
+            # TODO Think about if empty protein tables are allowed
+            raise NotImplementedError
+        self._set_columntypes()
 
-    def save(self, outfile, sep=";", index=False, **kwargs):
+    def _set_columntypes(self):
+        self.df.astype(
+            dtype={
+                "target_name": str,
+                "target_accession": str,
+                "tlen": int,
+                "query_name": str,
+                "query_accession": str,
+                "qlen": int,
+                "e-value": np.object_,
+                "score": np.object_,
+                "bias": np.object_,
+                "ndom": int,
+                "ndom_of": int,
+                "c-value": np.object_,
+                "i-value": np.object_,
+                "dom_score": np.object_,
+                "dom_bias": np.object_,
+                "hmm_from": int,
+                "hmm_to": int,
+                "ali_from": int,
+                "ali_to": int,
+                "env_from": int,
+                "env_to": int,
+                "acc": np.object_,
+                "UP": int,
+                "biome": str,
+                "CR": int,
+                "coverage_hit": np.object_,
+                "coverage_query": np.object_,
+                "similarity": np.object_,
+                "identity": np.object_,
+            }
+        )
+
+    def sort(self, by: Union[str, list], ascending: bool = False):
+        """
+        General method to sort protein table by column values
+        :param by:
+        :param ascending:
+        :return:
+        """
+        if not isinstance(by, list):
+            by = [by]
+        by = self._to_columnNames(by)
+        by.extend(["target_name", "ndom"])
+        # Set orientation according to setting, but always sort ndom ascending to keep domain order
+        orientation = [ascending if _ != "ndom" else True for _ in by]
+        self.df = self.df.sort_values(by=by, ascending=orientation)
+
+    def _to_columnNames(self, names: Union[str, list]) -> list:
+        """
+        method to remap inputs to the corresponding column names
+        :param names:
+        :return:
+        """
+        if not isinstance(names, list):
+            names = [names]
+        mapping = {colname: colname for colname in self.df.columns}
+        addins = {"eval": "score", "e-value": "score", "evalue": "score"}
+        mapping = {**mapping, **addins}
+        return list(map(mapping.get, names))
+
+    def save(self, outfile, sep=",", index=False, **kwargs):
+        """
+        save protein table to output (csv) fileS
+        :param outfile:
+        :param sep:
+        :param index:
+        :param kwargs:
+        :return:
+        """
         self.df.to_csv(outfile, sep=sep, index=index, **kwargs)
