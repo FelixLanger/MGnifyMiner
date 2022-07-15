67b5019ce1b13785f9ad02a20f651508eeb46cde
Felix Langer <felix@ebi.ac.uk>
2022-07-15 16:11:20 +0100

    add test for calculate_identity_similarity function

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,7 +1,9 @@
 import json
+import random
+import string
 from unittest import TestCase
 
-from mgyminer.parsers import extract_alignments
+from mgyminer.parsers import calculate_identity_similarity, extract_alignments
 from tests.fixtures import data_dir
 
 
@@ -10,3 +12,27 @@ def test_extract_alignments():
         expected_alignment = json.load(json_file)
     alignment = extract_alignments(data_dir / "phmmer_out.txt")
     TestCase().assertDictEqual(expected_alignment, alignment)
+
+
+def test_calculate_identity_similarity():
+    for i in range(10):
+        similar_aas = random.randint(0, 200)
+        identical_aas = random.randint(0, 200)
+        different_aas = random.randint(0, 200)
+        digits = random.randint(0, 5)
+        length = similar_aas + identical_aas + different_aas
+        consensus = (
+            random.choices(string.ascii_lowercase, k=identical_aas)
+            + ([" "] * different_aas)
+            + (["+"] * similar_aas)
+        )
+        random.shuffle(consensus)
+        consensus = "".join(consensus)
+        similarity_expected = round(
+            ((similar_aas + identical_aas) / length) * 100, digits
+        )
+        identity_expected = round((identical_aas / length) * 100, digits)
+        assert calculate_identity_similarity(consensus, digit=digits) == (
+            identity_expected,
+            similarity_expected,
+        )
