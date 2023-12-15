f281ce7be71a5b6021926e718217841a35cd4ba2
Felix Langer <felix@ebi.ac.uk>
2023-12-15 13:41:16 +0000

    use memory as cache

--- mgyminer/gui2/layouts/main_layout.py
+++ mgyminer/gui2/layouts/main_layout.py
@@ -4,9 +4,9 @@ from mgyminer.gui2.layouts import homepage
 layout = html.Div(
     [
         dcc.Location(id="url"),
-        dcc.Store(id="filter-parameters", storage_type="session"),
-        dcc.Store(id="filtered-data", storage_type="session"),
-        dcc.Store(id="selected-indices", storage_type="session"),
+        dcc.Store(id="filter-parameters", storage_type="memory"),
+        dcc.Store(id="filtered-data", storage_type="memory"),
+        dcc.Store(id="selected-indices", storage_type="memory"),
         # sidebar.sidebar,
         homepage.page,
     ]
