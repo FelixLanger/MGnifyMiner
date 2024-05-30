9e51fefb3a501e4a222e066d97ce2c5b925252d0
Felix Langer <felix@ebi.ac.uk>
2024-05-30 13:35:46 +0100

    add function to create md5 hash of a file for testing

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,9 +1,9 @@
 import hashlib
 
 
-def get_file_hash(file, ignore_comments=True):
+def calculate_text_md5(file, ignore_comments=True):
     """
-    Get the md5 hash of file without the # commented lines
+    Get the md5 hash of content in a file without the # commented lines
     :param ignore_comments: choose if lines commented out with # should be ignored
     :param file: file to hash
     :return: md5 hash digest
@@ -19,3 +19,18 @@ def get_file_hash(file, ignore_comments=True):
                 md5.update(line.encode("utf-8"))
 
     return md5.hexdigest()
+
+
+def calculate_md5(file_path):
+    """
+    calculate the md5 hash of a file
+    Args:
+        file_path: Path to file
+
+    Returns: md5sum
+    """
+    md5_hash = hashlib.md5()
+    with open(file_path, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            md5_hash.update(chunk)
+    return md5_hash.hexdigest()
